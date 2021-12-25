8b8e530f2e7ec1f752c6f1ffd6350ed2135a5617
chakane3 <39193557+chakane3@users.noreply.github.com>
2021-12-25 00:27:48 -0500

    init

---
+++ MealApp/MealApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealApp
+//
+//  Created by Chakane Shegog on 12/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
