c1911f2ce43e160e1b470733c588c8fd0d84e799
chakane3 <39193557+chakane3@users.noreply.github.com>
2021-12-25 22:59:57 -0500

    gathering data

---
+++ MealApp/MealApp/URLSession/MealAPI.swift
@@ -0,0 +1,8 @@
+//
+//  MealAPI.swift
+//  MealApp
+//
+//  Created by Chakane Shegog on 12/25/21.
+//
+
+import Foundation
---
+++ MealApp/MealApp/URLSession/NetworkServices.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkServices.swift
+//  MealApp
+//
+//  Created by Chakane Shegog on 12/25/21.
+//
+
+import Foundation
+
+
+// heres a list of things that could potentially go wrong when making requests
+enum NetworkError: Error, CustomStringConvertible
